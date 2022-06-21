f0d396f5628645faf9a6d1dd7f7a2e453531cf63
Pedro Plasencia <programacion.en.esp@gmail.com>
2022-06-21 23:13:11 +0200

    feat: status validation in select and improve in default user avatar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,15 +7,9 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UserStatusSelect } from './../interfaces/user-status-select';
+import {
+  DEFAULT_STATUS,
+  DEFAULT_STATUS_OPTIONS,
+} from '../constants/login-constants';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  public selectedStatus: string = DEFAULT_STATUS;
+  public statusOptions: UserStatusSelect[] = DEFAULT_STATUS_OPTIONS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
