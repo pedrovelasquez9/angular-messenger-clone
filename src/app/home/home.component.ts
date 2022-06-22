dcd65a40ffa84fb44cf9022d7ecd0c9f854fd994
Pedro Plasencia <programacion.en.esp@gmail.com>
2022-06-23 00:14:36 +0200

    feat: main chat toolbars and footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  DEFAULT_STATUS,
+  DEFAULT_STATUS_OPTIONS,
+} from './../constants/login-constants';
+import { UserStatusSelect } from './../interfaces/user-status-select';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public userStatus: string = DEFAULT_STATUS;
+  public statusOptions: UserStatusSelect[] = DEFAULT_STATUS_OPTIONS;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
